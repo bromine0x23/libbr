6971108d5f6079a29d530f89757493703bdf1416
bromine0x23 <bromine0x23@gmail.com>
2017-03-20 17:15:17 +0800

    Assert
    Signed-off-by: bromine0x23 <bromine0x23@gmail.com>
    Signed-off-by: bromine0x23 <bromine0x23@163.com>

--- libbr/assert.hpp
+++ libbr/assert.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 #include <libbr/assert/assert.hpp>
-#include <libbr/assert/assert_type_completeness.hpp>
+#include <libbr/assert/dummy_false.hpp>
+#include <libbr/assert/dummy_true.hpp>
