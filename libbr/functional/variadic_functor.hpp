6a034996277496a38ca5514bc7954371698f67e2
Bromine0x23 <bromine0x23@163.com>
2019-05-21 00:06:36 +0800

    functional/variadic-functor
    Signed-off-by: Bromine0x23 <bromine0x23@163.com>

---
+++ libbr/functional/variadic_functor.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief 变参函数对象基类
+ * @author Bromine0x23
+ */
+#pragma once
+
+#include <libbr/config.hpp>
+#include <libbr/functional/basic_functor.hpp>
+
+namespace BR {
+inline namespace Functional {
+
+/**
+ * @brief 变参函数对象基类
+ */
+struct VariadicFunctor : public BasicFunctor<> {
+	/**
+	 * @brief 元数
+	 */
+	constexpr static SInt arity = -1;
+
+	constexpr VariadicFunctor() noexcept = default;
+};
+
+} // namespace Functional
+} // namespace BR
