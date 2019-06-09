504a8bea58576f46d7def5d7f22bddbd12996b73
Bromine0x23 <bromine0x23@163.com>
2019-06-09 12:30:17 +0800

    operators
    Signed-off-by: Bromine0x23 <bromine0x23@163.com>

---
+++ libbr/operators.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief 运算符模块
+ * @author Bromine0x23
+ */
+#pragma once
+
+#include <libbr/config.hpp>
+
+namespace BR {
+
+/**
+ * @brief 运算符模块
+ */
+inline namespace Operators {
+} // namespace Operators
+
+} // namespace BR
